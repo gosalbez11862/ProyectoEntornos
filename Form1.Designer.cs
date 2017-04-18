2e2fcc1090cf802a486a7f945e7b7bf81b4191d1
gosalbez11862 <gosalbez11862@iesmarenostrum.com>
2017-04-18 19:27:35 +0200

    ---------------Versión NavajaSuiza II------------------------

---
+++ Form1.Designer.cs
@@ -0,0 +1,129 @@
+﻿namespace NavajaSuiza
+{
+    partial class NavajaSuiza
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Tema4 = new System.Windows.Forms.GroupBox();
+            this.Tema5 = new System.Windows.Forms.GroupBox();
+            this.Ejercicio4 = new System.Windows.Forms.Button();
+            this.Ejercicio13 = new System.Windows.Forms.Button();
+            this.Ejercicio2 = new System.Windows.Forms.Button();
+            this.Ejercicio3 = new System.Windows.Forms.Button();
+            this.Tema4.SuspendLayout();
+            this.Tema5.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // Tema4
+            // 
+            this.Tema4.Controls.Add(this.Ejercicio13);
+            this.Tema4.Controls.Add(this.Ejercicio4);
+            this.Tema4.Location = new System.Drawing.Point(13, 13);
+            this.Tema4.Name = "Tema4";
+            this.Tema4.Size = new System.Drawing.Size(342, 115);
+            this.Tema4.TabIndex = 0;
+            this.Tema4.TabStop = false;
+            this.Tema4.Text = "Tema 4";
+            // 
+            // Tema5
+            // 
+            this.Tema5.Controls.Add(this.Ejercicio2);
+            this.Tema5.Controls.Add(this.Ejercicio3);
+            this.Tema5.Location = new System.Drawing.Point(13, 134);
+            this.Tema5.Name = "Tema5";
+            this.Tema5.Size = new System.Drawing.Size(342, 115);
+            this.Tema5.TabIndex = 1;
+            this.Tema5.TabStop = false;
+            this.Tema5.Text = "Tema 5";
+            // 
+            // Ejercicio4
+            // 
+            this.Ejercicio4.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Ejercicio4.Location = new System.Drawing.Point(23, 32);
+            this.Ejercicio4.Name = "Ejercicio4";
+            this.Ejercicio4.Size = new System.Drawing.Size(110, 57);
+            this.Ejercicio4.TabIndex = 0;
+            this.Ejercicio4.Text = "Ejercicio 4 ";
+            this.Ejercicio4.UseVisualStyleBackColor = true;
+            // 
+            // Ejercicio13
+            // 
+            this.Ejercicio13.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Ejercicio13.Location = new System.Drawing.Point(200, 32);
+            this.Ejercicio13.Name = "Ejercicio13";
+            this.Ejercicio13.Size = new System.Drawing.Size(119, 57);
+            this.Ejercicio13.TabIndex = 1;
+            this.Ejercicio13.Text = "Ejercicio 13 ";
+            this.Ejercicio13.UseVisualStyleBackColor = true;
+            // 
+            // Ejercicio2
+            // 
+            this.Ejercicio2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Ejercicio2.Location = new System.Drawing.Point(23, 42);
+            this.Ejercicio2.Name = "Ejercicio2";
+            this.Ejercicio2.Size = new System.Drawing.Size(110, 57);
+            this.Ejercicio2.TabIndex = 3;
+            this.Ejercicio2.Text = "Ejercicio 2";
+            this.Ejercicio2.UseVisualStyleBackColor = true;
+            // 
+            // Ejercicio3
+            // 
+            this.Ejercicio3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Ejercicio3.Location = new System.Drawing.Point(200, 42);
+            this.Ejercicio3.Name = "Ejercicio3";
+            this.Ejercicio3.Size = new System.Drawing.Size(119, 57);
+            this.Ejercicio3.TabIndex = 2;
+            this.Ejercicio3.Text = "Ejercicio 3";
+            this.Ejercicio3.UseVisualStyleBackColor = true;
+            // 
+            // NavajaSuiza
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(367, 261);
+            this.Controls.Add(this.Tema5);
+            this.Controls.Add(this.Tema4);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "NavajaSuiza";
+            this.Text = "Navaja Suiza";
+            this.Tema4.ResumeLayout(false);
+            this.Tema5.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox Tema4;
+        private System.Windows.Forms.Button Ejercicio13;
+        private System.Windows.Forms.Button Ejercicio4;
+        private System.Windows.Forms.GroupBox Tema5;
+        private System.Windows.Forms.Button Ejercicio2;
+        private System.Windows.Forms.Button Ejercicio3;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace NavajaSuiza
+{
+    public partial class NavajaSuiza : Form
+    {
+        public NavajaSuiza()
+        {
+            InitializeComponent();
+        }
+    }
+}
