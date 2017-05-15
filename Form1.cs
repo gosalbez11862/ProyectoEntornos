a7ad34b0290c396aaaa033634908be804fb2d181
gosalbez11862 <gosalbez11862@iesmarenostrum.com>
2017-05-15 10:06:17 +0200

    Implementacion SandCastel_1

--- Form1.cs
+++ Form1.cs
@@ -20,7 +20,31 @@ using System.Windows.Forms;
 namespace NavajaSuiza
 {
     /// <summary>
-    /// Union de 4 ejercicios para conformar un proyecto de entornos
+    /// <para>Union de 4 ejercicios para conformar un proyecto de entornos</para>
+    /// <para>Los distintos ejercicios son:
+    /// <list type="bullet">
+    ///     <item>
+    ///         <description>Tema 4 - Ejercicio 4</description>
+    ///         <description>Conversor de Moneda, pasar de Pesetas
+    ///         a euros y al reves.</description>
+    ///     </item>
+    ///     <item>
+    ///         <description>Tema 4 - Ejercicio 13</description>
+    ///         <description>calcular la base dada con el 
+    ///         exponente dado.</description>
+    ///     </item>
+    ///     <item>
+    ///         <description>Tema 5 - Ejercicio 2</description>
+    ///         <description>Lectura de 10 numeros en un vector
+    ///         y nos devuelve la media de todo el vector.</description>
+    ///     </item>
+    ///     <item>
+    ///         <description>Tema 5 - Ejercicio 3</description>
+    ///         <description>Lectura de 10 numeros en un vector
+    ///         y nos devuelve el numero menor de todos ellos.</description>
+    ///     </item>
+    /// </list>
+    /// </para>
     /// </summary>
     public partial class Form1 : Form
     {
